a9d76257cf74d0f59b7664ee5c800c2e8b402141
yerin Jo <76177687+zunjune@users.noreply.github.com>
2022-03-04 00:23:25 +0900

    add main, join, join2, join3 responsive type scss
    add main, join, join2, join3 responsive type scss

---
+++ src/main/webapp/frontend/src/css/join.js
@@ -0,0 +1,90 @@
+import Header_1 from "./header";
+import Header_2 from "./header2";
+import { useNavigate } from 'react-router-dom';
+import React, { useState } from "react";
+import "./css/join.scss";
+import Register from "./img/register_title.png";
+
+function Join()
+{
+    let text = ["web test는 어쩌고 저쩌고...아직 못 정함",<br />,
+        "기반으로 한 공부하는 모두를 위한 공유 스터디서비스"]
+    const [userID, setId] = useState("");
+    const [password, setPw] = useState("");
+    const [pwCheck, setPwCheck] = useState("");
+    const [nickname, setNickName] = useState("");
+    const navigate = useNavigate();
+    return (
+        <div className="join1__outer">
+            <Header_1 />
+            <Header_2 />
+            <div className="join__block-01">
+                <img className="join__img-01" src={Register}></img>
+                <span className="join__block__text-01">회원가입</span>
+                <div className="join__block__text-02_03__block">
+                    <div className="join_block_text-02_03">
+                        <span className="join__block__text-02">Welcome!</span>
+                        <span className="join__block__text-03">
+                    웹 서비스 이용과 회원 등록을 위해 아래의 내용을 입력해주세요.</span>
+                    </div>
+                </div>
+
+ 
+                <div className="join__block-02">
+                    <span className="ID__text-02">아이디</span>
+                    <form>
+                        <input type="text" className="ID__input-02" onChange={(e)=>{setId(e.target.value)}}></input>
+                    </form>
+                    <span className="ID__text-02">비밀번호</span>
+                    <form>
+                        <input type="password" className="ID__input-02" onChange={(e)=>{setPw(e.target.value)}}></input>
+                    </form>
+                    <span className="ID__text-02">비밀번호 확인</span>
+                    <form>
+                        <input type="password" className="ID__input-02" onChange={(e)=>{setPwCheck(e.target.value)}}></input>
+                    </form>
+                    <span className="ID__text-02">닉네임</span>
+                    <form>
+                        <input type="text" className="ID__input-02" onChange={(e)=>{setNickName(e.target.value)}}></input>
+                    </form>
+                    <div className="join__block__button-01">
+                        <span className="join__block__button__text" onClick={()=>{check()}}>다음</span>
+                    </div>
+                </div>
+            </div>
+            <div className="join__block-03">
+                <span className="join__block__text-04">web test</span>
+                <span className="join__block__text-05">{text}</span>
+            </div>
+        </div>
+    );
+    function check()
+    {
+        if(userID=="")
+        {
+            alert("아이디를 입력해주세요.");
+        }
+        else if(password=="")
+        {
+            alert("비밀번호를 입력해주세요.");
+        }
+        else if(password.length<8 || password.length>20)
+        {
+            alert("비밀번호는 8자 이상 20자 이하로 입력해주세요.");
+        }
+        else if(nickname=="")
+        {
+            alert("닉네임을 입력해주세요.");
+        }
+        else if(pwCheck!==password)
+        {
+            alert("비밀번호가 일치하지 않습니다.");
+        }
+        else
+        {
+            navigate('/join2', {state:{userID:userID, password:password, nickname:nickname}});
+        }
+    }
+}
+
+export default Join;
---
+++ src/main/webapp/frontend/src/css/main.js
@@ -0,0 +1,78 @@
+import Header from "./header";
+import Header2 from "./header2";
+import "./css/main.scss";
+import ddaom from "./img/ddaom1.png";
+import arrow from "./img/arrow.png";
+import { Link } from "react-router-dom";
+function Main() {
+    let text_01 = ["나만의 공부메이트와 함께해요.",<br/>]
+    let text_02 = ["가치풀자와 차근차근 함께 성장할 수 있어요."]
+    let text_03 = ["가치풀자면 충분해!",<br/>,"가치풀자로 정보공유와 풀이 Q&A까지."]
+    let text_04 = ["끝.",<br/>]
+    return (
+        <div className="main" style={{overflowY: 'scroll'}}>
+            <Header />
+            <Header2 />
+            <div className="main-01">
+                <div className="main__img">
+                <img src={ddaom} className="main__ddaom"></img>
+                </div>
+                <div className="main__text__block-01">
+                    <span className="main__text-01">보영</span>
+                    <span className="main__text-02">님,<br/></span>
+                    <span className="main__text-03">이제 혼자하는 공부는 </span>
+                    <span className="main__text-03-1"> &nbsp; {text_04}</span>
+                </div>
+                <div className="main__bar"></div>
+                <div className="main__text__block-02">
+                    <span>{text_04}<br/></span>
+                    <span>{text_01}<br/></span>
+                    <span>{text_02}</span>
+                </div>
+                <div className="mobile-button">
+                         <div className="button__text-01">가치풀자와 함께하고 싶다면?</div>
+                         <div className="button__text-02">
+                             <div className="button__text-03">문제 풀러가기</div>
+                         </div>
+
+                </div>
+            </div>
+            <div className="main-02">
+                <span className="main__hash">#</span>
+                <span className="main__dot-01">·</span>
+                <span className="main__dot-02">·</span>
+                <span className="main__text-04">영어부터</span>
+                <span className="main__text-05">전공까지</span>
+                <div className="main__text__block-04">
+                <span className="main__text-06">{text_03}</span>
+                </div>
+                <Link to="/main2">
+                <div className="main__button">
+                    <span className="main__button__text">더 찾아보기</span>
+                </div>
+                </Link>
+                <div className="circle-01">
+                    <span className="circle__text__number-01">1</span>
+                    <span className="circle__text__subject">토익</span>
+                </div>
+                <div className="circle-02">
+                    <span className="circle__text__number-02">2</span>
+                    <span className="circle__text__subject">토플</span>
+                </div>
+                <div className="circle-03">
+                    <span className="circle__text__number-03">3</span>
+                    <span className="circle__text__subject">교양</span>
+                </div>
+                <div className="circle-04">
+                    <span className="circle__text__number-04">4</span>
+                    <span className="circle__text__subject">전공</span>
+                </div>
+                <img className="arrow" src={arrow}></img>
+            </div>
+
+        </div>
+        
+    );
+  }
+  
+  export default Main
